d6fd0a80e64ce63076b4e06140b939652621efbd
bvignau <benjamin.vignau@insa-cvl.fr>
2023-02-23 14:48:52 +0100

    add logos

--- src/components/About/Partenaires.js
+++ src/components/About/Partenaires.js
@@ -5,6 +5,11 @@ import { ReactComponent as LIFO_logo } from '../../Assets/logo_lifo.svg';
 import { ReactComponent as PEPITE_logo } from '../../Assets/logo_pepite.svg';
 import { ReactComponent as INSA_logo } from '../../Assets/INSA.svg';
 
+import { ReactComponent as INPI_logo } from '../../Assets/inpi.svg';
+import { ReactComponent as CVALO_logo } from '../../Assets/c_valo.svg';
+import { ReactComponent as DEVUP_logo } from '../../Assets/cci.svg';
+import { ReactComponent as CCI_logo } from '../../Assets/dev_up.svg';
+
 
 
 
@@ -26,6 +31,23 @@ function Partenaires() {
       <Col xs={4} md={2} className="tech-icons">
         <PEPITE_logo />
       </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <INPI_logo />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <CVALO_logo />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DEVUP_logo />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <CCI_logo />
+      </Col>
+      
     </Row>
   );
 }
