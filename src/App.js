014e9f8cee90949ab80ceb2dba8895efcd25bc1d
bvignau <benjamin.vignau@insa-cvl.fr>
2023-02-23 13:31:46 +0100

    try fix router

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/website" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
