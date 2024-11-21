c018f118ac7cab4197b458d111a65dd00b989141
bvignau <benjamin.vignau@insa-cvl.fr>
2024-11-21 10:31:40 +0100

    fix image and add CNAME

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: 'https://www.coeur-net.fr/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
