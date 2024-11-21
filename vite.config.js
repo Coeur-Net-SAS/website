4eabdd012be0d504dea1140bc799b4c6e49810bf
bvignau <benjamin.vignau@insa-cvl.fr>
2024-11-21 10:50:39 +0100

    try to fix router

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/repo/',
+  base: '/website/',
   server: {
     host: true,
   },
