aa355de693402377ea0cd486d1ca775cd5df8e05
joseph.kawalec@coeur-net.fr <joseph.kawalec@coeur-net.fr>
2025-04-03 17:54:22 +0200

    ajout des assets manquants

---
+++ assets/img/partners/index.js
@@ -0,0 +1,33 @@
+import bpifrance from "./bpifrance.png"
+import lifo from "./lifo.png"
+import insa_cvl from "./insa_cvl.png"
+import pepite from "./pepite.png"
+import inpi from "./inpi.png"
+import cvalo from "./cvalo.png"
+import cci_cher from "./cci_cher.png"
+import region from "./region.png"
+import entreprendre from "./entreprendre.png"
+import vierzon from "./vierzon.png"
+import b3 from "./b3.png"
+import devup from "./devup.png"
+import ovh from "./OVHcloud.png"
+import reseau_initiative from "./CVL_Reseau_Initiative.png"
+import UE from "./UE.png"
+
+export {
+  bpifrance,
+  lifo,
+  insa_cvl,
+  pepite,
+  inpi,
+  cvalo,
+  cci_cher,
+  region,
+  entreprendre,
+  vierzon,
+  b3,
+  devup,
+  ovh,
+  reseau_initiative,
+  UE
+};
